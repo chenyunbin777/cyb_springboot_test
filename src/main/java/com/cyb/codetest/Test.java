9bed845d27b66766defa90f52f02b9da98751f71
chenyunbin <chenyunbin@souche.com>
2020-12-22 15:54:49 +0800

    add:LinkedList

--- src/main/java/com/cyb/codetest/Test.java
+++ src/main/java/com/cyb/codetest/Test.java
@@ -1,12 +1,14 @@
 package com.cyb.codetest;
 
 import java.util.HashMap;
+import java.util.LinkedList;
 
 public class Test {
     public static void main(String[] args) {
         System.out.println();
 
         HashMap hashmap = new HashMap();
+        LinkedList linkedList = new LinkedList();
 
     }
 }
